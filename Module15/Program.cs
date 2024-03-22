78cb9ffaea050173b874cc7596a08b1ebf83a1f6
Nikita Nikita <jassie.jassie@yandex.ru>
2024-03-22 14:37:24 +0300

    Отложенное и немедленное выполнение

--- Module15/Program.cs
+++ Module15/Program.cs
@@ -8,64 +8,20 @@ class Program
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
-
-
     }
 
-    static void GroupingByKey(List<Contact> contacts)
+    static void DelayedExecuting(List<Student> students)
     {
-        var emailsGrouped = from contact in contacts
-                            group contact by contact.EMail.Split("@").Last() into TwoGroups
-                            where TwoGroups.Key != "example.com"
-                            select TwoGroups;
+        var youngStudents = from s in students where s.Age < 25 select s;
 
-        foreach (var e in emailsGrouped)
-        {
-            Console.WriteLine($"Ключ {e.Key}:");
-            foreach (var element in e)
-                Console.WriteLine($"\t{element.EMail}");
-            Console.WriteLine();
-        }
+        //Выполнится, когда мы обратимся к переменной youngStudents для получения результата
     }
 
-    static void JoinMethod(List<Car> cars, List<Manufacturer> manufacturers)
+    static void InstantExecuting(List<Student> students)
     {
-        var result = from car in cars
-                     join m in manufacturers on car.Manufacturer equals m.Name
-                     select new
-                     {
-                         Name = car.Model,
-                         Manufacturer = m.Name,
-                         m.Country
-                     };
+        var youngStudents = (from s in students
+                            where s.Age < 25
+                            select s).ToList();
 
-        var result2 = cars.Join(manufacturers,
-            car => car.Manufacturer,
-            m => m.Name,
-            (car, m) => new
-            {
-                Name = car.Model,
-                Manufacturer = m.Name,
-                m.Country
-            });
-    }
-
-    static void GroupJoinMethod(List<Car> cars, List<Manufacturer> manufacturers)
-    {
-        var result = manufacturers.GroupJoin(cars, m => m.Name, car => car.Manufacturer,
-            (m, cars) => new
-            {
-                m.Name,
-                m.Country,
-                Cars = cars.Select(s => s.Model)
-            });
-
-        foreach (var item in result)
-        {
-            Console.WriteLine($"{item.Name} --> {item.Country}");
-            foreach (var car in item.Cars)
-                Console.WriteLine($"{car}");
-            Console.WriteLine();
-        }
     }
 }
